25430be2e2dd73c0cc57768d99dd83acac205c2f
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-06-09 11:37:27 +0600

    start my portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#FFB400",
+          secondary: "#090306",
+          accent: "#111111",
+          neutral: "#FFFFFF",
+          error: "#FA5C5C",
+        },
+      },
+      {
+        dark: {
+          primary: "#72B626",
+          secondary: "#D3D3D3",
+          accent: "#FFFFFF",
+          neutral: "#F3F4F6",
+          error: "#FB7185",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
